c08b4231bd1b7b57a00e17d5c052784c36d7163d
Keneke-Einar <khabdrazakovkenesary@gmail.com>
2025-07-22 17:40:59 +0500

    data-drop-in: Add initial data insertion for students table

---
+++ data-drop-in.sql
@@ -0,0 +1,4 @@
+INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
+    ('Mustafa', 'Sarvarov', 'mustafa.sarvarov@gmail.com', '2023-10-04'),
+    ('Arthur', 'Kim', 'arthur.kim@alem.school', '2024-10-04'),
+    ('Elnar', 'Moldabelov', 'elnar.moldabekov@doodocs.net', '2023-01-16');
