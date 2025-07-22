cf5d752bf717bdf1f7319af0571216f92f96a1a3
kekhabdra <khabdrazakovkenesary@gmail.com>
2025-07-22 20:15:57 +0500

    In order to ensure the security and continuing stability, the Republic will be reorganized into the first Galactic Empire! For a safe and secure society!

---
+++ column-commander.sql
@@ -0,0 +1,12 @@
+SELECT first_name, last_name
+FROM employees
+WHERE department_id = 1
+ORDER BY first_name;
+
+SELECT email AS contact_info, position AS job_title
+FROM employees
+WHERE position = 'Lead Engineer';
+
+SELECT first_name || ' ' || last_name AS given_name, salary AS monthly_income
+FROM employees
+ORDER BY salary, given_name DESC;
