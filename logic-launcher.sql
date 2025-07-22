43c2cb94a2f7fa0821b7874991579c1b24b32de9
Keneke-Einar <khabdrazakovkenesary@gmail.com>
2025-07-22 21:20:58 +0500

    fix

--- logic-launcher.sql
+++ logic-launcher.sql
@@ -27,11 +27,7 @@ ORDER BY first_name DESC;
 
 SELECT first_name, last_name, position, salary, department_id, hire_date
 FROM employees
-WHERE department_id = (
-    SELECT department_id
-    FROM departments
-    WHERE name = 'Analytics'
-) 
+WHERE position LIKE '%Analyst%'
 AND hire_date BETWEEN '2012-01-01' AND '2016-12-31'
 AND salary < 70000
 ORDER BY first_name DESC;
