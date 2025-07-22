b47239dcd5b99208d71fbe52dd2d1d8e54df0009
Keneke-Einar <khabdrazakovkenesary@gmail.com>
2025-07-22 17:43:04 +0500

    data-drop-in: Fix email address for Arthur Kim in students data drop-in

--- data-drop-in.sql
+++ data-drop-in.sql
@@ -1,4 +1,4 @@
 INSERT INTO students (student_id, first_name, last_name, email, enrollment_date) VALUES
     ('1', 'Mustafa', 'Sarvarov', 'mustafa.sarvarov@gmail.com', '2023-10-04'),
-    ('2', 'Arthur', 'Kim', 'arthur.kim@alem.school', '2024-10-04'),
+    ('2', 'Arthur', 'Kim', 'arthur.kim@mail.ru', '2024-10-04'),
     ('3', 'Elnar', 'Moldabelov', 'elnar.moldabekov@doodocs.net', '2023-01-16');
