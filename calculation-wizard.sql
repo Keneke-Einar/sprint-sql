9fcde364533b219ef6a8ec47900a4d3959f1aa31
kekhabdra <khabdrazakovkenesary@gmail.com>
2025-07-22 20:35:19 +0500

    calculation-wizard: calculation wizard: initial query to calculate employee salaries and tenure

---
+++ calculation-wizard.sql
@@ -0,0 +1,2 @@
+SELECT first_name || ' ' || last_name AS full_name, salary * 12 AS annual_salary, salary * 1.1 AS increased_salary, ROUND((julianday(CURRENT_TIMESTAMP) - julianday(hire_date)) / 365.25) AS years_employed, salary / 20 AS daily_rate
+FROM employees
