c6403f5b0287e22c3cb65ad88814d6fe3dc555d2
kekhabdra <khabdrazakovkenesary@gmail.com>
2025-07-23 20:53:11 +0500

    fix

--- having-havoc.sql
+++ having-havoc.sql
@@ -1,5 +1,5 @@
 SELECT department_id, COUNT(*) AS employee_count, ROUND(AVG(salary), 3) AS average_salary
 FROM employees
 GROUP BY department_id
-HAVING employee_count > 2 AND average_salary < '75000'
+HAVING employee_count > 2 AND average_salary < 75000
 ORDER BY department_id
