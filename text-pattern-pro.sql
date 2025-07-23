e2c8b7afd7336b80f25b6c0082e137846cd8cc14
kekhabdra <khabdrazakovkenesary@gmail.com>
2025-07-23 17:36:34 +0500

    fix

--- text-pattern-pro.sql
+++ text-pattern-pro.sql
@@ -1,7 +1,7 @@
 SELECT 
 CASE
     WHEN budget > '1000000' THEN UPPER(name) || ' - LARGE BUDGET'
-    WHEN budget BETWEEN '500000' AND '1000000' THEN UPPER(SUBSTR(name,1,1)) || ' - Medium'
+    WHEN budget BETWEEN '650001' AND '1000000' THEN UPPER(SUBSTR(name,1,1)) || ' - Medium'
     ELSE LOWER(name) || ' - small budget'
 END AS department_display
 FROM departments
