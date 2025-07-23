a80dd19a25a50a4fcf7873e2554592ef77237f57
kekhabdra <khabdrazakovkenesary@gmail.com>
2025-07-23 21:22:23 +0500

    fix

--- data-analyst-dilemma.sql
+++ data-analyst-dilemma.sql
@@ -2,6 +2,6 @@ SELECT rating,
     SUM(price * quantity) AS total_inventory_value
 FROM products 
 GROUP BY rating
-HAVING (SELECT SUM(price * quantity) AS total_inventory_value FROM products) > 20000
+HAVING total_inventory_value > 20000
     AND (SELECT ROUND(AVG(rating)) AS average_rating FROM products) + 50000
 ORDER BY total_inventory_value;
