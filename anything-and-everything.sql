974e4b9d94aaffa9360a132a47bf8e76c8a126bb
Keneke-Einar <khabdrazakovkenesary@gmail.com>
2025-07-24 18:38:39 +0500

    anything-and-everything

---
+++ anything-and-everything.sql
@@ -0,0 +1,17 @@
+SELECT product_id, name, price
+FROM products
+WHERE price > (
+    SELECT MIN(price)
+    FROM products
+    WHERE rating BETWEEN 4.5 AND 4.6
+)
+ORDER BY price DESC;
+
+SELECT product_id, name, price
+FROM products
+WHERE price > (
+    SELECT MIN(price)
+    FROM products
+    WHERE rating BETWEEN 4.1 AND 4.2
+)
+ORDER BY price DESC;
