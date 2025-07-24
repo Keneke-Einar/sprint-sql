0a81e5d1023f32eccb934062d0d5191d4511b5b5
Keneke-Einar <khabdrazakovkenesary@gmail.com>
2025-07-24 18:45:20 +0500

    fix

--- anything-and-everything.sql
+++ anything-and-everything.sql
@@ -10,7 +10,7 @@ ORDER BY price DESC;
 SELECT product_id, name, price
 FROM products
 WHERE price > (
-    SELECT MIN(price)
+    SELECT MAX(price)
     FROM products
     WHERE rating BETWEEN 4.1 AND 4.2
 )
