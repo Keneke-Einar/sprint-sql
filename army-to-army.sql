3f7a59de6322167d40c0da5d1075fda4dd2a8b41
kekhabdra <khabdrazakovkenesary@gmail.com>
2025-07-28 19:55:15 +0500

    army-to-army

---
+++ army-to-army.sql
@@ -0,0 +1,5 @@
+SELECT playlists.Name AS PlaylistName, tracks.Name AS TrackName
+FROM playlists
+JOIN playlist_track ON playlist_track.PlaylistId = playlists.PlaylistId
+JOIN tracks ON tracks.TrackId = playlist_track.TrackId
+ORDER BY PlaylistName AND TrackName;
